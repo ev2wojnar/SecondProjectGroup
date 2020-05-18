8adc5e3c5a7bbaa58c3c3cf5c6c99e060748a931
Krzysztof Wojnar <frugomaster@gmail.com>
2020-05-18 17:51:17 +0200

    cinema example data & Service

--- src/main/java/com/example/demo/SecondGroupProjectApplication.java
+++ src/main/java/com/example/demo/SecondGroupProjectApplication.java
@@ -26,6 +26,12 @@ public class SecondGroupProjectApplication {
                 "Andrzej Poziomka, Tadeusz Litwin, Admin Admin", 2.3, 14.5);
         movieRepository.save(protosi);
         movieRepository.save(admin123);
+        Cinema kinoX = new Cinema(1, "Kino X", new Address("Wrocław", "Pawianicka", "34", "21-370"),
+                new CinemaHall(3, 170));
+        Cinema kinoY = new Cinema(2, "Kino Y", new Address("Zabrze", "Bąbelkowa", "108", "79-654"),
+                new CinemaHall(6, 90));
+        cinemaRepository.save(kinoX);
+        cinemaRepository.save(kinoY);
     }
 
 }
