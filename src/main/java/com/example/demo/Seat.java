d7eb5828ac5ec7e44665a22ab4402a60a61f8eef
Krzysztof Wojnar <frugomaster@gmail.com>
2020-05-20 01:24:59 +0200

    adding reservation class

--- src/main/java/com/example/demo/Seat.java
+++ src/main/java/com/example/demo/Seat.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
+
 public class Seat {
 
-    char row;
-    int position;
+    private char row;
+    private int position;
     //@Override
     public boolean equals(Seat another) {
         if (another.row == this.row && another.position == this.position) return true;
