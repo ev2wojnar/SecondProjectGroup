ecc97ce28acf2529b47a39659ca3e7c18c81506d
Krzysztof Wojnar <frugomaster@gmail.com>
2020-05-29 18:32:35 +0200

    customer & reservation controller & service

---
+++ src/main/java/com/example/demo/ctrls/CustomerController.java
@@ -0,0 +1,20 @@
+package com.example.demo.ctrls;
+
+import com.example.demo.model.Customer;
+import com.example.demo.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("cinemas/customers/{customerId}")
+    public Customer findCustomer(@PathVariable long customerId){
+        return customerService.findCustomerById(customerId);
+    }
+}
