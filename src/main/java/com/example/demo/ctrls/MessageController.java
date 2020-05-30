29090f844d1a194793d942b7ddad2fd3d6bc5453
kasiachy <kasia.chrobak7@gmail.com>
2020-05-30 08:57:15 +0200

    some of the names changed, trying thymeleaf

--- src/main/java/com/example/demo/ctrls/MessageController.java
+++ src/main/java/com/example/demo/ctrls/MessageController.java
@@ -1,16 +1,22 @@
 package com.example.demo.ctrls;
 
+import com.example.demo.model.Movie;
+import com.example.demo.model.MovieGenre;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
+
 @Controller
 public class MessageController {
+    Movie up = new Movie ("Odlot", "70-letni Carl po śmierci żony zamienia swój dom w statek powietrzny i odlatuje do Ameryki Południowej, by spełnić swoje marzenie. Przez przypadek zabiera ze sobą ośmioletniego skauta.",
+            MovieGenre.ANIMATION.FAMILY.COMEDY.ADVENTURE, LocalDate.of(2009,5,13), "USA", "Pete Docter, Bob Peterson", "Wojciech Siemion, Ignacy Gogolewski",96 , 12.5);
 
     @GetMapping("/welcome")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="You") String name, Model model) {
+        model.addAttribute("name", up.getDescription() );
         return "welcome";
     }
 
