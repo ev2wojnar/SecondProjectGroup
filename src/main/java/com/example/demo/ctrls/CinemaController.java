d942470dd2fc48c6f73e8aa1e3428e2af778238b
Krzysztof Wojnar <frugomaster@gmail.com>
2020-06-04 13:39:26 +0200

    added th: endpoint to all menu bars

--- src/main/java/com/example/demo/ctrls/CinemaController.java
+++ src/main/java/com/example/demo/ctrls/CinemaController.java
@@ -4,7 +4,6 @@ import com.example.demo.model.Cinema;
 import com.example.demo.services.CinemaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -39,6 +38,19 @@ public class CinemaController {
     public void deleteCinema(@PathVariable long cinemaId){
         cinemaService.deleteCinemaById(cinemaId);
     }
+//    @GetMapping("/priceList")
+//    public String priceList(@RequestParam(name="price", required=false, defaultValue="0")String name, Model model){
+//        //model.addAttribute("price", model);
+//        return "priceList";
+//    }
+//    @GetMapping("/index")
+//    public String showMain(@RequestParam(name="index2", required=false, defaultValue="1")String name, Model model){
+//        return "index";
+//    }
+//    @GetMapping("/happyHours")
+//    public String happyHours(@RequestParam(name="happyHours", required=false, defaultValue="gfggfg")String name, Model model){
+//        return "sale";
+//    }
 
 
 
